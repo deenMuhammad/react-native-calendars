b2f08340e76e08738d1812fd7115599be90d126b
Dinmukhammad Rakhimov <40787426+deenMuhammad@users.noreply.github.com>
2020-01-29 18:00:58 +0900

    Update index.js

--- src/calendar/day/multi-dot/index.js
+++ src/calendar/day/multi-dot/index.js
@@ -60,7 +60,7 @@ class Day extends Component {
   }
   renderDot(marking){
     if(marking.level>=1&&marking.level<2){
-      return <View style={{width: '100%', height: '50%', backgroundColor: '#0041c5', opacity: 0.4}}/>
+      return <View style={{width: '100%', height: '50%', backgroundColor: 'transparent'}}/>
     }
     if(marking.level>=2&&marking.level<3){
       return <View style={{width: '100%', height: '50%', backgroundColor: '#0041c5'}}/>
